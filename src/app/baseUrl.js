9937ac0346c9afeee24142e19ea95891046baee0
ahmed abdo <130068607+ahmed824@users.noreply.github.com>
2025-01-21 20:37:43 +0200

    Update baseUrl.js

--- src/app/baseUrl.js
+++ src/app/baseUrl.js
@@ -1,5 +1,5 @@
-export const baseUrl = "http://192.168.0.201:8080/safer"
-// export const baseUrl = "https://safer.deltawy.com"
+// export const baseUrl = "http://192.168.0.201:8080/safer"
+export const baseUrl = "https://safer.deltawy.com"
 
 
 // export const imageUrl = "http://192.168.0.201:8080/safer/imag?id="
